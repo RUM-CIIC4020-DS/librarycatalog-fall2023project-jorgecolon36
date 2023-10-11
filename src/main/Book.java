0bc17f91a380f0caa8304f3209f9c7a09ff4f7d6
Jorge Colón <jorge.colon25@upr.edu>
2023-10-11 18:15:44 -0400

    Missing JavaDoc

--- src/main/Book.java
+++ src/main/Book.java
@@ -1,44 +1,76 @@
 package main;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class Book {
 	
+	// Private variables:
+	private int id;
+	private String title;
+	private String author;
+	private String genre;
+	private LocalDate lastCheckOut;
+	private boolean checkedOut;
+	
+	
+	
 	public int getId() {
-		return -10;
+		return id;
 	}
 	public void setId(int id) {
-		
+		this.id = id;
 	}
 	public String getTitle() {
-		return "";
+		return title;
 	}
 	public void setTitle(String title) {
-		
+		this.title = title;
 	}
 	public String getAuthor() {
-		return "";
+		return author;
 	}
 	public void setAuthor(String author) {
-		
+		this.author = author;
 	}
 	public String getGenre() {
-		return "";
+		return genre;
 	}
 	public void setGenre(String genre) {
-		
+		this.genre = genre;
 	}
 	public LocalDate getLastCheckOut() {
-		return null;
+		return lastCheckOut;
 	}
 	public void setLastCheckOut(LocalDate lastCheckOut) {
-		
+		this.lastCheckOut = lastCheckOut;
 	}
 	public boolean isCheckedOut() {
+		if(checkedOut) {
+			return true;
+		}
 		return false;
 	}
 	public void setCheckedOut(boolean checkedOut) {
-		
+		this.checkedOut = checkedOut;
+	}
+	
+	public Book() {
+		this.id = 0;
+		this.title = "";
+		this.author = "";
+		this.genre = "";
+		this.lastCheckOut = LocalDate.of(2000, 01, 1);
+		this.checkedOut = true;
+	}
+	
+	public Book(int id, String title, String author, String genre, LocalDate lastCheckOut, boolean checkedOut) {
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.genre = genre;
+		this.lastCheckOut = lastCheckOut;
+		this.checkedOut = checkedOut;
 	}
 	
 	@Override
@@ -50,12 +82,24 @@ public class Book {
 		 * 
 		 * Both the title and author are in uppercase.
 		 */
-		return "";
+		String title = getTitle().toUpperCase();
+		String author = getAuthor().toUpperCase();
+		
+		return title + " BY " +author;
 	}
 	public float calculateFees() {
 		/*
 		 * fee (if applicable) = base fee + 1.5 per additional day
 		 */
-		return -1000;
+		if(isCheckedOut() == false) {
+			return 0;
+		}
+		LocalDate today = LocalDate.of(2023, 9, 15);
+		
+		long days = getLastCheckOut().until(today,ChronoUnit.DAYS);
+		if(days>=31) {
+			return (float) ((days-31)*1.5 + 10);
+		}
+		return 0;
 	}
 }
--- src/main/LibraryCatalog.java
+++ src/main/LibraryCatalog.java
@@ -7,6 +7,8 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.stream.Collectors;
+
 
 import data_structures.ArrayList;
 import data_structures.DoublyLinkedList;
@@ -16,43 +18,168 @@ import interfaces.List;
 
 public class LibraryCatalog {
 	
-		
+	private List<Book> Catalog = new ArrayList<>();
+	private List<User> Users = new ArrayList<>();
+	
 	public LibraryCatalog() throws IOException {
+		this.Catalog = getBooksFromFiles();
+		this.Users = getUsersFromFiles();
 		
 	}
 	private List<Book> getBooksFromFiles() throws IOException {
-		return null;
+
+		List<Book> books = new ArrayList<Book>();
+		BufferedReader br = new BufferedReader(new FileReader("data"+"/catalog.csv"));
+		
+		String line = "";  
+		String splitBy = ",";  
+		line = br.readLine();
+//		String[] tempCI = line.split(splitBy);
+//		Book book1 = new Book(0, tempCI[1],tempCI[2],tempCI[3],LocalDate.now(), true);
+//		books.add(book1);
+		try   {
+			while ((line = br.readLine()) != null) {
+				String[] catalogInfo = line.split(splitBy);
+				Book book = new Book(Integer.parseInt(catalogInfo[0]), catalogInfo[1],catalogInfo[2],catalogInfo[3],LocalDate.parse(catalogInfo[4]), Boolean.parseBoolean(catalogInfo[5]));
+				books.add(book);
+			}
+		}
+		catch (IOException e)   {
+			e.printStackTrace();  
+		}	
+		return books;
 	}
 	
 	private List<User> getUsersFromFiles() throws IOException {
-		return null;
+		
+		// Creates variables
+		List<User> users = new ArrayList<User>();
+
+		BufferedReader br = new BufferedReader(new FileReader("data"+"/user.csv"));
+		String line = "";  
+		String splitBy = ",";  
+		line = br.readLine();
+		// Reads csv files
+		try   {
+			while ((line = br.readLine()) != null) {
+				List<Book> books = new ArrayList<Book>();
+				String[] catalogInfo = line.split(splitBy);
+				if(catalogInfo.length == 2) {
+					User user = new User(Integer.parseInt(catalogInfo[0]), catalogInfo[1], books);
+					users.add(user);
+				}
+				
+				else {
+					// Removes curly braces
+					String cleanedInput = catalogInfo[2].substring(1, catalogInfo[2].length() - 1);
+					
+					// Devides numbers in spaces
+					String[] numberStrings = cleanedInput.split(" ");
+					
+					// Adds books owned to books ArrayList
+					for (String numberString : numberStrings) {
+						int number = Integer.parseInt(numberString);
+						
+						for(Book book: getBooksFromFiles()) {
+							if(book.getId()==number) {
+								books.add(book);
+								break;
+							}
+						}
+					}
+					
+					User user = new User(Integer.parseInt(catalogInfo[0]), catalogInfo[1], books);
+					users.add(user);
+				}
+			}
+		}
+		catch (IOException e)   {
+			e.printStackTrace();  
+		}	
+		return users;
 	}
+	
 	public List<Book> getBookCatalog() {
-		return null;
+		
+		List<Book> books = new ArrayList<Book>();
+		for(Book book: this.Catalog) {
+			books.add(book);
+		}
+		return books;
 	}
+	
 	public List<User> getUsers() {
-		return null;
+		
+		List<User> users = new ArrayList<User>();
+		try {
+			for(User user: getUsersFromFiles()) {
+				users.add(user);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return users;
 	}
 	public void addBook(String title, String author, String genre) {
+		Book book = new Book(this.Catalog.size()+1, title, author, genre, LocalDate.of(2023, 9, 15), false);
+		this.Catalog.add(book);
 		return;
 	}
 	public void removeBook(int id) {
+		for(Book book:this.Catalog) {
+			if(book.getId() == id) {
+				this.Catalog.remove(book);
+			}
+		}
 		return;
 	}	
 	
 	public boolean checkOutBook(int id) {
-		return true;
+		for(Book book: this.Catalog) {
+			if(!book.isCheckedOut() && book.getId() == id) {
+				book.setLastCheckOut(LocalDate.of(2023, 9, 15));
+				book.setCheckedOut(true);
+				return true;
+			}
+		}
+		return false;
 	}
 	public boolean returnBook(int id) {
-		return true;
+		for(Book book: this.Catalog) {
+			if(book.isCheckedOut() && book.getId() == id) {
+				book.setCheckedOut(false);
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	public boolean getBookAvailability(int id) {
-		return true;
+		
+//		for(Book book: this.Catalog) {
+//			if(!book.isCheckedOut() && book.getId() == id) {
+//				return true;
+//			}
+//		}
+//		return false;
+		for(Book book: this.Catalog) {
+			if(book.getId() == id) {
+				return book.isCheckedOut();
+			}
+		}
+		return false;
 	}
 	public int bookCount(String title) {
-		return 1000;
+		int count = 0;
+		for(Book book:this.Catalog) {
+			if(book.getTitle().equals(title)) {
+				count++;
+			}
+		}
+		return count;
 	}
+	
 	public void generateReport() throws IOException {
 		
 		String output = "\t\t\t\tREPORT\n\n";
@@ -69,13 +196,13 @@ public class LibraryCatalog {
 		 * How you do the count is up to you. You can make a method, use the searchForBooks()
 		 * function or just do the count right here.
 		 */
-		output += "Adventure\t\t\t\t\t" + (/*Place here the amount of adventure books*/) + "\n";
-		output += "Fiction\t\t\t\t\t\t" + (/*Place here the amount of fiction books*/) + "\n";
-		output += "Classics\t\t\t\t\t" + (/*Place here the amount of classics books*/) + "\n";
-		output += "Mystery\t\t\t\t\t\t" + (/*Place here the amount of mystery books*/) + "\n";
-		output += "Science Fiction\t\t\t\t\t" + (/*Place here the amount of science fiction books*/) + "\n";
+		output += "Adventure\t\t\t\t\t" + String.valueOf(searchForBooks("Adventure")) + "\n";
+		output += "Fiction\t\t\t\t\t\t" + String.valueOf(searchForBooks("Fiction")) + "\n";
+		output += "Classics\t\t\t\t\t" + String.valueOf(searchForBooks("Classics")) + "\n";
+		output += "Mystery\t\t\t\t\t\t" + String.valueOf(searchForBooks("Mystery")) + "\n";
+		output += "Science Fiction\t\t\t\t\t" + String.valueOf(searchForBooks("Science Fiction")) + "\n";
 		output += "====================================================\n";
-		output += "\t\t\tTOTAL AMOUNT OF BOOKS\t" + (/*Place here the total number of books*/) + "\n\n";
+		output += "\t\t\tTOTAL AMOUNT OF BOOKS\t" + String.valueOf(this.Catalog.size()) + "\n\n";
 		
 		/*
 		 * This part prints the books that are currently checked out
@@ -90,10 +217,17 @@ public class LibraryCatalog {
 		 * 
 		 * PLACE CODE HERE
 		 */
+		int count = 0;
+		for(Book book: this.Catalog) {
+			if(book.isCheckedOut()) {
+				output += book.toString()+"\n";
+				count++;
+			}
+		}
 		
 		
 		output += "====================================================\n";
-		output += "\t\t\tTOTAL AMOUNT OF BOOKS\t" (/*Place here the total number of books that are checked out*/) + "\n\n";
+		output += "\t\t\tTOTAL AMOUNT OF BOOKS\t" + String.valueOf(count) + "\n\n";
 		
 		
 		/*
@@ -115,10 +249,21 @@ public class LibraryCatalog {
 		 * 
 		 * PLACE CODE HERE!
 		 */
+		float total = 0;
+		for(User user: getUsers()) {
+			float userTotal = 0;
+			for(Book book: user.getCheckedOutList()) {
+				total+= book.calculateFees();
+				userTotal+= book.calculateFees();
+			}
+			if(!user.getCheckedOutList().isEmpty()) {
+				output += user.getName() + "\t\t\t\t\t" +"$"+ String.format("%.2f", userTotal)+ "\n";
+			}
+		}
 
 			
 		output += "====================================================\n";
-		output += "\t\t\t\tTOTAL DUE\t$" + (/*Place here the total amount of money owed to the library.*/) + "\n\n\n";
+		output += "\t\t\t\tTOTAL DUE\t$" + String.format("%.2f", total) + "\n\n\n";
 		output += "\n\n";
 		System.out.println(output);// You can use this for testing to see if the report is as expected.
 		
@@ -129,9 +274,23 @@ public class LibraryCatalog {
 		 * 
 		 * PLACE CODE HERE!!
 		 */
+		File report = new File ("report.txt");
+		report.createNewFile();
+		BufferedWriter writer = new BufferedWriter(new FileWriter("report/report.txt"));
+		writer.write(output);
+		writer.close();
 		
 	}
-	
+	public int searchForBooks(String genre) {
+		int result = 0;
+		for(Book book: this.Catalog) {
+			if(book.getGenre().equals(genre)) {
+				result++;
+			}
+		}
+		
+		return result;
+	}
 	/*
 	 * BONUS Methods
 	 * 
@@ -139,7 +298,15 @@ public class LibraryCatalog {
 	 * other parts of the project.
 	 */
 	public List<Book> searchForBook(FilterFunction<Book> func) {
-		return null;
+//        List<Book> result = new ArrayList<>();
+//        for (Book book : getBookCatalog()) {
+//            if (func.filter(book)) {
+//                result.add(book);
+//            }
+//        }
+//        return result;
+		List<Book> filteredBooks = searchForBook(book -> book.getGenre().equals("Mystery"));
+		return filteredBooks;
 	}
 	
 	public List<User> searchForUsers(FilterFunction<User> func) {
